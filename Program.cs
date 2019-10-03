93c2129df3579c64ad30e184431c809a17baee5f
Bearshark16 <54946080+Bearshark16@users.noreply.github.com>
2019-10-03 14:44:31 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prov___modellering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
